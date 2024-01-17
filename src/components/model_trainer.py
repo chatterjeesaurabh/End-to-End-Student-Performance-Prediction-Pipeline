0e16ccb297b30b537e0f86731b31e51f479d190b
Saurabh <chatterjeesaurabh38@gmail.com>
2024-01-17 19:47:22 +0530

    added hyperparameter tuning in model trainer and utils

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,7 +47,52 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
-            model_report:dict = evaluate_models(X_train, y_train, X_test, y_test, models)   # R2 Score  (definition in 'utils.py')
+            # HYPERPARAMETER TUNING: test parameters for Grid Search Cross-Validation
+            params={            
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [5,7,9,11],
+                    # 'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['ball_tree', 'kd_tree', 'brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
+            model_report:dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)   # R2 Score  (definition in 'utils.py')
 
             # Get best model score from dictionary 'model_report':
             best_model_score = max(sorted(model_report.values()))
@@ -62,7 +107,7 @@ class ModelTrainer:
 
             logging.info(f"Best model found in both train and test dataset: [{best_model_name}] with R2 Score = {best_model_score}")
 
-            save_object(
+            save_object(            # save the best model as 'PKL' file in the 'artifacts' folder
                 file_path = self.model_trainer_config.trained_model_file_path,
                 obj = best_model
             )
--- src/utils.py
+++ src/utils.py
@@ -3,31 +3,38 @@ import sys
 
 import numpy as np
 import pandas as pd
-import dill                # helps saving the 'preprocessing' object as 'PKL' file
+import dill                # helps saving the any object in a location
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV       # for using different hyperparameters on each models
 
 from src.exception import CustomException
 
 
-def save_object(file_path, obj):
+def save_object(file_path, obj):       # Save an object 'obj' to location with file name defined in 'file_path'
     try:
-        dir_path = os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)       # parent folder name
 
-        os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)        # create the folder
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)            
+            dill.dump(obj, file_obj)                # save the object into the folder
     
     except Exception as e:
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):      # Train model on train dataset, then evaluate model performance (R2-Score) in test dataset
     try:
-        report = {}
+        report = {}         # Dictionary of all model's R2-score performance
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]       # parameters for grid-search of hyper-parameters
+
+            gs = GridSearchCV(model, para, cv=3)        # 'Grid Search Cross-Validation' object prepared
+            gs.fit(X_train, y_train)                    # performing grid-search of hyper-parameters on all models
+
+            model.set_params(**gs.best_params_)         # set model parameters to the best ones obtained from the Grid search
 
             model.fit(X_train, y_train) # Train model
 
@@ -36,8 +43,8 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             y_test_pred = model.predict(X_test)
             
             # Evaluate Train and Test dataset
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)     # R2-score on Train data
+            test_model_score = r2_score(y_test, y_test_pred)        # R2-score on Test data
 
             report[list(models.keys())[i]] = test_model_score
         
